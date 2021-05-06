f91b50fad7d4102a2a50f729be5fb0a3f6c4974a
vale2904 <70981266+vale2904@users.noreply.github.com>
2021-05-05 19:15:34 -0500

    search
    se agrega busqueda datos

--- scripts/conexion.js
+++ scripts/conexion.js
@@ -2,29 +2,77 @@ let  busqueda="Love Is War ";
 
 let url=`http://api.giphy.com/v1/gifs/search?api_key=fEAR9TrqgjatwcOtKAPuEkthH7dg1Z9k&q=${busqueda}`;
 
+//2. Definir los elementos del barra de busqueda
 let botonBusqueda=document.getElementById("botonBusqueda");
-let parametroBusqueda=document.getElementById("Busqueda");
+let parametroBusqueda=document.getElementById("busqueda");
 
-botonBusqueda=addEventListener("click",buscarDatos);
+//3. Agregar el evento de clic al boton busqueda
+botonBusqueda.addEventListener("click",buscarDatos);
+
+//4. LLamar a la función conectarGiphy
+conectarGiphy(1);
+
+
 
 function buscarDatos(){
-    console.log("hola estoy buscando....");
+
+    busqueda=parametroBusqueda.value;
+    url=`http://api.giphy.com/v1/gifs/search?api_key=fEAR9TrqgjatwcOtKAPuEkthH7dg1Z9k&limit=9&q=${busqueda}`;
+
+    conectarGiphy();
+
 }
 
-fetch(url)
-    .then(respuesta=>respuesta.json())
-    .then(datos=>organizarDatos(datos));
+function conectarGiphy(condicion){
 
-    function organizarDatos(datos){
+    if(condicion==1){
+        
+        fetch(url)
+        .then(respuesta=>respuesta.json())
+        .then(datos=>organizarDatos(datos));
 
-      let contenedorPadre=document.getElementById("contenedor");
-      let arreglosDatos=datos.data;
-
-      arreglosDatos.map(function(dato){
-          let imagen=document.createElement("img");
-
-          imagen.src=dato.images.downsized_medium.url;
-          contenedorPadre.appendChild(imagen);
-      });
+    }else{
+        fetch(url)
+        .then(respuesta=>respuesta.json())
+        .then(datos=>organizarBusqueda(datos));
     }
+}
+
+function organizarDatos(datos){
+
+    let contenedorPadre=document.getElementById("contenedor");
+    let arregloDatos=datos.data;
+
+    arregloDatos.map(function(dato){
+        
+        let imagen=document.createElement("img");
+
+        imagen.src=dato.images.downsized_medium.url;
+        imagen.classList.add("p-5");
+        contenedorPadre.appendChild(imagen);
+
+    });
+
+}
+
+function organizarBusqueda(datos){
     
+    let contenedorPadre=document.getElementById("contenedor");
+    
+    while(contenedorPadre.firstChild){
+        contenedorPadre.removeChild(contenedorPadre.firstChild);
+    }
+
+    let arregloDatos=datos.data;
+
+    arregloDatos.map(function(dato){
+        
+        let imagen=document.createElement("img");
+
+        imagen.src=dato.images.downsized_medium.url;
+        imagen.classList.add("p-5");
+        contenedorPadre.appendChild(imagen);
+
+    });
+
+}
